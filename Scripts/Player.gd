393684089d37961488efedd14b29f4dd29614b4d
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-05-30 20:36:08 +1000

    Updated Player Camera Rotation
    - Updated player camera rotation to horizontally rotate the player body instead of just the head.
    - Made the 3 boxes in the Game map metallic and reflective.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,9 +4,20 @@ extends CharacterBody3D
 var speed
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 8.0
 
-const SENSITIVITY = 0.01
+# Camera movement variables
+var rotation_input : float
+var tilt_input : float
+var mouse_rotation : Vector3
+
+var player_rotation : Vector3
+var camera_rotation : Vector3
+
+const MIN_CAMERA_TILT := deg_to_rad(-90)
+const MAX_CAMERA_TILT := deg_to_rad(90)
+
+const SENSITIVITY = 0.5
 
 # Head bob variables
 const BOB_FREQ = 2.0
@@ -20,38 +31,46 @@ const FOV_CHANGE = 1.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
-
+@export var head : Node3D
+@export var camera : Camera3D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+
+func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		rotation_input = -event.relative.x * SENSITIVITY
+		tilt_input = -event.relative.y * SENSITIVITY
+		
+		#head.rotate_y(-event.relative.x * SENSITIVITY)
+		#camera.rotate_x(-event.relative.y * SENSITIVITY)
+		#camera.rotation.x = clamp(camera.rotation.x, MIN_CAMERA_TILT, MAX_CAMERA_TILT)
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
+	
+	# Handle camera movement
+	update_camera(delta)
+	
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	#Handle sprint.
+	
+	#Handle sprint
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	# Get the input direction and handle the movement/deceleration
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
@@ -62,7 +81,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 4.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 4.0)
-
+	
 	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = head_bob(t_bob)
@@ -73,6 +92,21 @@ func _physics_process(delta):
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	
 	move_and_slide()
+
+func update_camera(delta):
+	mouse_rotation.y += rotation_input * delta
+	mouse_rotation.x += tilt_input * delta
+	mouse_rotation.x = clamp(mouse_rotation.x, MIN_CAMERA_TILT, MAX_CAMERA_TILT)
+	
+	player_rotation = Vector3(0.0, mouse_rotation.y, 0.0)
+	camera_rotation = Vector3(mouse_rotation.x, 0.0, 0.0)
+	
+	global_transform.basis = Basis.from_euler(player_rotation)
+	head.transform.basis = Basis.from_euler(camera_rotation)
+	head.rotation.z = 0.0
+	
+	rotation_input = 0.0
+	tilt_input = 0.0
 
 func head_bob(time) -> Vector3:
 	var pos = Vector3.ZERO
