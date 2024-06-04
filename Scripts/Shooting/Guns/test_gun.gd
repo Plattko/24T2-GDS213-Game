465b91d1723223d436ebef6bcf4a9b44573a9f02
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-06-04 20:27:13 +1000

    Added Bullet Holes
    - Imported a bullet hole texture.
    - Created the Bullet Decal scene which is a Decal node.
    - Updated the Test Gun script to spawn a bullet hole where the player shoots. A maximum of 30 bullet holes are visible before the newest bullet hole deletes the oldest.

--- Scripts/Shooting/Guns/test_gun.gd
+++ Scripts/Shooting/Guns/test_gun.gd
@@ -3,8 +3,12 @@ extends Node3D
 @export var anim_player : AnimationPlayer
 
 var raycast_test = preload("res://Scenes/raycast_test.tscn")
+var bullet_decal = preload("res://Scenes/bullet_decal.tscn")
 
 const RAY_RANGE := 2000.0
+
+var decal_queue = []
+const MAX_QUEUE_SIZE := 30
 
 func _physics_process(delta):
 	# Handle shooting
@@ -30,7 +34,7 @@ func shoot() -> void:
 		
 		if result:
 			print("Hit: " + result.collider.name)
-			test_raycast(result.get("position"))
+			spawn_decal(result.get("position"), result.get("normal"))
 		else:
 			print ("Hit nothing.")
 
@@ -40,3 +44,29 @@ func test_raycast(position: Vector3) -> void:
 	instance.global_position = position
 	await get_tree().create_timer(3.0).timeout
 	instance.queue_free()
+
+func spawn_decal(position: Vector3, normal: Vector3) -> void:
+	# Instantiate bullet decal
+	var instance = bullet_decal.instantiate()
+	# Make it a child of the scene
+	get_tree().root.add_child(instance)
+	# Set its position
+	instance.global_position = position
+	
+	instance.look_at(instance.global_transform.origin + normal, Vector3.UP)
+	
+	if normal != Vector3.UP and normal != Vector3.DOWN:
+		instance.rotate_object_local(Vector3(1, 0, 0), 90)
+	
+	update_decal_queue(instance)
+	
+	## Destroy the decal after 3 seconds
+	#await get_tree().create_timer(3.0).timeout
+	#instance.queue_free()
+
+func update_decal_queue(decal):
+	decal_queue.push_back(decal)
+	
+	if decal_queue.size() > MAX_QUEUE_SIZE:
+		var decal_to_destroy = decal_queue.pop_front()
+		decal_to_destroy.queue_free()
