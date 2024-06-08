c6299e451f60aeaf6b3be28e872b8ed29a0ce800
JamesCass <136556196+JamesCass@users.noreply.github.com>
2024-06-08 16:25:59 +1000

    Implemented simple AI behavior
    - Implemented simple enemy AI with functionality to move around the nav mesh towards the player.
    - Included base signal functionality for attacking player within range.
    - Included base signal functionality for avoidance behavior around other AI agents.

---
+++ Scripts/AI/enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+@onready var nav_agent = $NavigationAgent3D
+
+var SPEED = 3.0
+
+# Set new velocity 
+func _physics_process(delta):
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * SPEED
+	
+	nav_agent.set_velocty(new_velocity)
+
+# Move agent towards target
+func update_target_location(target_location):
+	nav_agent.set_target_position(target_location)
+
+# Signal for handling when enemy is close to player
+func _on_navigation_agent_3d_target_reached():
+	print("player in enemy attack range")
+
+# Signal for handling avoidance behavior with other agents
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	velocity = velocity.move_toward(safe_velocity, .25)
+	move_and_slide()
