28081abf4a97abbfe55a35292c8aff6aa87f59cb
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-06-09 19:14:43 +1000

    Added Damaging/Killing Enemies
    - Added Damageable to Enemy scene. Bullets can currently be unintentionally blocked by the enemy's other collision shape.
    - Updated Enemy script to manage the enemy's health and destroy them when it reaches 0.

--- Scripts/AI/enemy.gd
+++ Scripts/AI/enemy.gd
@@ -1,9 +1,23 @@
 extends CharacterBody3D
 
-
 @onready var nav_agent = $NavigationAgent3D
 
+# Movement variables
 var SPEED = 3.0
+
+# Health variables
+var max_health := 100
+var cur_health
+
+func _ready():
+	cur_health = max_health
+	
+	for child in get_children():
+		if child is Damageable:
+			# Connect each damageable to the damaged signal
+			child.damaged.connect(on_damaged)
+		else:
+			push_warning("Object contains non-damageable child node.")
 
 # Set new velocity 
 func _physics_process(delta):
@@ -25,3 +39,9 @@ func _on_navigation_agent_3d_target_reached():
 func _on_navigation_agent_3d_velocity_computed(safe_velocity):
 	velocity = velocity.move_toward(safe_velocity, .25)
 	move_and_slide()
+
+func on_damaged(damage: float):
+	cur_health -= damage
+	
+	if cur_health <= 0:
+		queue_free()
