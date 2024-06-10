d89c50936cc460dc02156eefb0cf76b912120323
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-06-10 15:11:38 +1000

    Started Modular Weapon System
    - Saved the Test Gun's material as test_rifle.
    - Created new Weapon Testing scene with no enemies/navmesh and a cleaner scene tree.
    - Duplicated Weapon Testing scene as a Testing Base Level scene.
    - Created Rifle scene as a first weapon.
    - Created a custom Weapon resource script that will act as a base resource to create weapons with and holds data such as the weapon's position and mesh.
    - Created Rifle resource.
    - Created the Initialise Weapon script that loads the weapon's variables using the provided weapon resource.

---
+++ Scripts/CustomResources/weapon.gd
@@ -0,0 +1,12 @@
+class_name Weapon
+
+extends Resource
+
+@export var name : StringName
+
+@export_category("Weapon Orientation Settings")
+@export var position : Vector3
+@export var rotation : Vector3
+
+@export_category("Visual Settings")
+@export var mesh : Mesh
