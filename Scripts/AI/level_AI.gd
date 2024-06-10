f0a11178d75577f65b1ffa6d2504aa7971908fd5
JamesCass <136556196+JamesCass@users.noreply.github.com>
2024-06-10 16:05:04 +1000

    Implemented functionality that prevents enemies from spawning at any spawn points that the player can currently see
    - Created a dedicated scene for the spawn points as opposed to Node3D's within the level in order to attach the VisibleOnScreenNotifier3D node
    - Attached a script to the Spawn Point scene that contained the signals from the VisibleOnScreenNotifier3D to recognize what spawn points were visible to the player
    - Implemented functionality into the Level_AI script that will only allow enemies to be spawned at spawn points not visible to the player otherwise a print will fire and the spawn will attempt again next timer completion.

--- Scripts/AI/level_AI.gd
+++ Scripts/AI/level_AI.gd
@@ -18,15 +18,24 @@ func _physics_process(delta):
 	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
 	
 
-# Get a random spawn point that is a child to the "EnemySpawns" node
-func _get_random_child(parent_node):
-	var random_id = randi() % parent_node.get_child_count()
-	return parent_node.get_child(random_id)
+# Get a list of spawn points that are not visible on screen
+func _get_available_spawn_points():
+	var available_spawn_points = [] # Create an empty array to hold all of the available spawn points 
+	for i in range(enemy_spawns.get_child_count()): # Loop through each child of the "enemy_spawns node"
+		var spawn_point = enemy_spawns.get_child(i) 
+		if spawn_point.is_available_for_spawn(): # Checks if the spawn point is not visible on screen
+			available_spawn_points.append(spawn_point) # If not visible then add it to the list of available spawn points 
+	return available_spawn_points # Return the list of spawn points
 
-# When timer ends signal to instansiate new enemy
+# When timer ends signal to instantiate new enemy at any available spawn points or print "no available spawn points"
 func _on_enemy_spawn_timer_timeout():
-	var spawn_point = _get_random_child(enemy_spawns).global_position
-	instance = enemy.instantiate()
-	instance.position = spawn_point
-	navigation_region.add_child(instance)
-	print("Enemy spawned")
+	var available_spawn_points = _get_available_spawn_points() # Get the list of any available spawn points
+	if available_spawn_points.size() > 0:
+		var random_id = randi() % available_spawn_points.size() # Get a random index from the available spawn points
+		var spawn_point = available_spawn_points[random_id].global_position # Get the global position of the randomly selected spawn point 
+		instance = enemy.instantiate()
+		instance.position = spawn_point
+		navigation_region.add_child(instance)
+		print("Enemy spawned")
+	else:
+		print("No available spawn points")
