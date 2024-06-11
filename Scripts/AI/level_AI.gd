77597699191216ca692a7a62deb779c746666002
JamesCass <136556196+JamesCass@users.noreply.github.com>
2024-06-11 20:52:47 +1000

    Started setting up wave based gamemode
    - Began setting up initial wave based game mode
    - Included base pseudocode for overall implementation idea

--- Scripts/AI/level_AI.gd
+++ Scripts/AI/level_AI.gd
@@ -5,18 +5,25 @@ extends Node3D
 @onready var enemy_spawns = $EnemySpawnNodes/EnemySpawns
 @onready var navigation_region = $NavigationRegion3D
 
-
 var enemy = load("res://Scenes/enemy.tscn")
 var instance 
 
-# Creates a seed for a random number generator
+# Wave variables
+var current_wave
+var next_wave
+
+# Enemy count variables
+var alive_enemies
+var max_enemies
+
+# Creates a seed for a random number generator and set the initial current wave
 func _ready():
 	randomize()
+	current_wave = 1
 
 # Direct agents in group "enemies" to the player
 func _physics_process(delta):
 	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
-	
 
 # Get a list of spawn points that are not visible on screen
 func _get_available_spawn_points():
@@ -28,6 +35,7 @@ func _get_available_spawn_points():
 	return available_spawn_points # Return the list of spawn points
 
 # When timer ends signal to instantiate new enemy at any available spawn points or print "no available spawn points"
+#func spawn_enemy():
 func _on_enemy_spawn_timer_timeout():
 	var available_spawn_points = _get_available_spawn_points() # Get the list of any available spawn points
 	if available_spawn_points.size() > 0:
@@ -37,5 +45,19 @@ func _on_enemy_spawn_timer_timeout():
 		instance.position = spawn_point
 		navigation_region.add_child(instance)
 		print("Enemy spawned")
+		spawn_next_wave()# TESTING
 	else:
 		print("No available spawn points")
+
+#somehow check all enemies have been defeated
+#start wait timer when all enemies are defeated
+func start_wait_timer():
+	pass
+	#set the next waves max enemies to be increased from last wave
+	#when timer ends spawn the next wave
+
+func spawn_next_wave():
+	current_wave = current_wave + 1
+	print("current wave: " + str(current_wave))
+	#spawn the current max enemies at spawn points that are counted as alive enemies
+	#possilble foreach loop to spawn the correct amount of max_enemies at the spawn points?
