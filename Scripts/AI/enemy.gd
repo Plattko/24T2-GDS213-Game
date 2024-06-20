d301ec88965f870396b8b5c39332a7da7498e104
JamesCass <136556196+JamesCass@users.noreply.github.com>
2024-06-20 21:54:13 +1000

    Implemented custom enemy mesh and animations, fixed player teleport bug, created new "enemy-testing" level, added simple obstacles & lighting to level
    - Imported custom enemy mesh with animations for running, attacking & spawning.
    - Fixed bug regarding player being teleported when not moving and first wave is spawned.
    - Added some basic geometry and lights to a newly made level to test enemies, close some sight lines and test light types

--- Scripts/AI/enemy.gd
+++ Scripts/AI/enemy.gd
@@ -1,11 +1,18 @@
 extends CharacterBody3D
 
 @onready var nav_agent = $NavigationAgent3D
+@onready var animation_tree = $AnimationTree
+
+const ATTACK_RANGE = 1.5
 
 # Movement variables
-var min_speed := 3.0
-var max_speed := 6.0
+var min_speed := 4.0
+var max_speed := 7.0
 var speed
+
+var player : Player
+
+var anim_state_machine
 
 # Health variables
 var max_health := 100
@@ -16,6 +23,7 @@ var damage := 20
 signal enemy_defeated
 
 func _ready():
+	anim_state_machine = animation_tree.get("parameters/playback")
 	cur_health = max_health
 	speed = randf_range(min_speed, max_speed)
 	
@@ -28,18 +36,39 @@ func _ready():
 
 # Set new velocity 
 func _physics_process(delta):
-	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_path_position()
-	var new_velocity = (next_location - current_location).normalized() * speed
-	nav_agent.set_velocity(new_velocity)
+	
+	match anim_state_machine.get_current_node():
+		"Run":
+			var current_location = global_transform.origin
+			var next_location = nav_agent.get_next_path_position()
+			var new_velocity = (next_location - current_location).normalized() * speed
+			nav_agent.set_velocity(new_velocity)
+			look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z), Vector3.UP)
+		"Attack":
+			nav_agent.set_velocity(Vector3.ZERO)
+			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	
+	animation_tree.set("parameters/conditions/attack", _target_in_range())
+	animation_tree.set("parameters/conditions/run", !_target_in_range())
+	
+	animation_tree.get("parameters/playback")
 	
 	if cur_health <= 0:
 		enemy_defeated.emit()
 		queue_free()
 
+func _target_in_range():
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
+
 # Move agent towards target
 func update_target_location(target_location):
-	nav_agent.set_target_position(target_location)
+	if anim_state_machine.get_current_node() == "Run":
+		nav_agent.set_target_position(target_location)
+
+func initialise(player_ref : Player):
+	player = player_ref
+	
 
 # Signal for handling when enemy is close to player
 func _on_navigation_agent_3d_target_reached():
@@ -60,3 +89,9 @@ func _on_area_3d_area_entered(area):
 
 func _on_enemy_defeated():
 	pass
+
+func _hit_finished():
+	var children = player.get_children()
+	for child in children:
+		if child.name == "Damageable":
+			child.take_damage(damage)
