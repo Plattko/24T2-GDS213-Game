67d39387dec8534738fdefc1622df0af40361bfe
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-06-29 14:30:07 +1000

    Added New Multiplayer Connection Scene
    - Created Multiplayer Connection Menu scene where the player can enter a username and click Host New Game, Join as Player 2 or Start Game.
    - Updated Multiplayer Manager script to no longer be an autoload and instead be attached to the root node in the Multiplayer Connection Menu scene.
    - Updated Multiplayer Manager script to make the lifecycle callback connections in _ready instead of when the player hosts a new game.
    - Updated naming convention of all functions with a connection.
    - Updated _on_host_new_game_pressed (formerly become_host) function in Multiplayer Manager script to no longer set the player spawn node, remove the singleplayer player or manually call on_peer_connected (formerly add_player_to_game).
    - Commented out the old functionality in the Game Manager script, made it an autoload, and updated it to store player information in a dictionary.
    - Updated Multiplayer Manager script to store and distribute each player's information such as their name and ID in the Game Manager when a player connects to the server.
    - Updated Multiplayer Manager to instantiate the game scene when a player clicks Start Game.
    - Updated _on_host_new_game_pressed function in Multiplayer Manager to manually pass in the host's player information.
    - Removed the temporary multiplayer menu in the Network Testing scene.
    - Temporarily commented out the server check in the Player Input script.

--- Scripts/Gamemode/game_manager.gd
+++ Scripts/Gamemode/game_manager.gd
@@ -1,13 +1,16 @@
 extends Node
 
-@export var multiplayer_hud : Control
+var players = {}
 
-func become_host() -> void:
-	print("Pressed Host New Game.")
-	multiplayer_hud.hide()
-	MultiplayerManager.become_host()
-
-func join_as_player_2() -> void:
-	print("Pressed Join as Player 2.")
-	multiplayer_hud.hide()
-	MultiplayerManager.join_as_player_2()
+#------------------------------OLD MULTIPLAYER----------------------------------
+#@export var multiplayer_hud : Control
+#
+#func become_host() -> void:
+	#print("Pressed Host New Game.")
+	##multiplayer_hud.hide()
+	##MultiplayerManager.become_host()
+#
+#func join_as_player_2() -> void:
+	#print("Pressed Join as Player 2.")
+	##multiplayer_hud.hide()
+	##MultiplayerManager.join_as_player_2()
--- Scripts/Multiplayer/multiplayer_manager.gd
+++ Scripts/Multiplayer/multiplayer_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var name_line : LineEdit
+
 # Server variables
 const SERVER_PORT := 8080
 const SERVER_IP = "127.0.0.1"
@@ -14,11 +16,21 @@ var multiplayer_player = preload("res://Scenes/Multiplayer/multiplayer_player.ts
 
 var players_spawn_node
 
-func become_host() -> void:
+func _ready():
+	# Connect lifecycle callbacks
+	multiplayer.peer_connected.connect(on_peer_connected) # Calls this function when a peer connects to the server
+	multiplayer.peer_disconnected.connect(on_peer_disconnected)
+	multiplayer.connected_to_server.connect(on_connected_to_server)
+	multiplayer.connection_failed.connect(on_connection_failed)
+
+#---------------------------------BUTTONS---------------------------------------
+## Formerly called become_host
+func _on_host_new_game_pressed() -> void:
 	print("Starting host.")
-	# Establishes the player spawn node - Server-side only
-	players_spawn_node = get_tree().current_scene.get_node("Players")
+	## Establishes the player spawn node - Server-side only
+	#players_spawn_node = get_tree().current_scene.get_node("Players")
 	
+	##--------------------------------------------------------------------------
 	# Create a new peer
 	server_peer = ENetMultiplayerPeer.new()
 	# Check for an error creating the server
@@ -33,19 +45,26 @@ func become_host() -> void:
 	multiplayer.set_multiplayer_peer(server_peer) #multiplayer.multiplayer_peer = server_peer
 	print("Waiting for players!")
 	
-	# Lifecycle callbacks
-	multiplayer.peer_connected.connect(add_player_to_game) # Calls this function when a peer connects to the server
-	multiplayer.peer_disconnected.connect(remove_player_from_game)
+	# Pass in the host's player information
+	send_player_information(multiplayer.get_unique_id(), name_line.text)
+	##--------------------------------------------------------------------------
 	
-	is_multiplayer = true
+	## Lifecycle callbacks
+	#multiplayer.peer_connected.connect(add_player_to_game) # Calls this function when a peer connects to the server
+	#multiplayer.peer_disconnected.connect(remove_player_from_game)
+	#multiplayer.connected_to_server.connect(connected_to_server)
+	#multiplayer.connection_failed.connect(connection_failed)
 	
-	remove_singleplayer_player()
+	#is_multiplayer = true
 	
-	# Manually add the player to the server because peer connected isn't called
-	add_player_to_game(1)
+	#remove_singleplayer_player()
+	
+	## Manually add the player to the server because peer connected isn't called
+	#add_player_to_game(1)
 
-func join_as_player_2() -> void:
-	print("Player 2 joining.")
+func _on_join_as_player_2_pressed() -> void:
+	#print("Player 2 joining.")
+	##--------------------------------------------------------------------------
 	# Create a new peer
 	var client_peer = ENetMultiplayerPeer.new()
 	# Make it a client of the chosen server
@@ -54,39 +73,74 @@ func join_as_player_2() -> void:
 	client_peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	# Set yourself as the multiplayer peer
 	multiplayer.set_multiplayer_peer(client_peer) #multiplayer.multiplayer_peer = client_peer
+	##--------------------------------------------------------------------------
 	
-	remove_singleplayer_player()
+	#remove_singleplayer_player()
 
+func _on_start_game_pressed() -> void:
+	# Call start_game on all peers
+	start_game.rpc()
+
+#--------------------------------CONNECTIONS------------------------------------
+## Formerly called add_player_to_game
 # Is called on the server and clients when someone connects
-func add_player_to_game(id: int) -> void:
+func on_peer_connected(id: int) -> void:
 	print("Player %s connected!" % id)
 	
-	# Instantiate the multiplayer player scene
-	var player_to_add = multiplayer_player.instantiate()
+	## Instantiate the multiplayer player scene
+	#var player_to_add = multiplayer_player.instantiate()
 	
-	# Set the player's ID and name
-	player_to_add.player_id = id
-	player_to_add.name = str(id)
+	## Set the player's ID and name
+	#player_to_add.player_id = id
+	#player_to_add.name = str(id)
 	
-	players_spawn_node.add_child(player_to_add)
+	#players_spawn_node.add_child(player_to_add)
 
+## Formerly called remove_player_from_game
 # Is called on the server and clients when someone disconnects
-func remove_player_from_game(id: int) -> void:
+func on_peer_disconnected(id: int) -> void:
 	print("Player %s disconnected!" % id)
 	
-	var player_to_remove = players_spawn_node.get_node(str(id))
-	if player_to_remove:
-		player_to_remove.queue_free()
-		player_to_remove = null
+	#var player_to_remove = players_spawn_node.get_node(str(id))
+	#if player_to_remove:
+		#player_to_remove.queue_free()
+		#player_to_remove = null
 
 # Is called only from clients
 # If you want to send information from the client to the server, do it from here
-func connected_to_server() -> void: 
+func on_connected_to_server() -> void: 
 	print("Connected to server.")
+	# Send connected player's information to the server
+	send_player_information.rpc_id(1, multiplayer.get_unique_id(), name_line.text)
 
 # Is called only from clients
-func connection_failed() -> void: 
+func on_connection_failed() -> void: 
 	print("Connection failed.")
+
+#-------------------------------------------------------------------------------
+@rpc("any_peer", "call_local")
+func start_game() -> void:
+	# Load the scene
+	var scene = load("res://Scenes/Levels/Testing/network-testing.tscn").instantiate()
+	# Add it to our tree
+	get_tree().root.add_child(scene)
+	# Hide the connection menu
+	self.visible = false
+
+## NOTE: Use this if players select weapon loadout before entering game
+@rpc("any_peer")
+func send_player_information(id, name) -> void:
+	# If the player doesn't already exist, send the player information to the server
+	if !GameManager.players.has(id):
+		GameManager.players[id] = {
+			"id" : id,
+			"name" : name,
+		}
+	
+	# Pass the player information from the server to every peer
+	if multiplayer.is_server():
+		for n in GameManager.players:
+			send_player_information.rpc(n, GameManager.players[n].name)
 
 func remove_singleplayer_player() -> void:
 	print("Remove singleplayer Player.")
