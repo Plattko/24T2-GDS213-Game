3808360fb49ae3505877c0805ff37e9a6bb024c4
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-07-04 20:17:32 +1000

    Housekeeping
    - Removed constant print in Hitmarker script.
    - Renamed two variables in Hitmarker script.

--- Scripts/UI/hitmarker.gd
+++ Scripts/UI/hitmarker.gd
@@ -3,8 +3,8 @@ extends CenterContainer
 
 @export var reg_anim_player : AnimationPlayer
 @export var crit_anim_player : AnimationPlayer
-@export var reg_hit_markers : Array[Line2D] = []
-@export var crit_hit_markers : Array[Line2D] = []
+@export var reg_hitmarkers : Array[Line2D] = []
+@export var crit_hitmarkers : Array[Line2D] = []
 
 var recent_dmg : float
 var max_recent_dmg := 200.0
@@ -16,26 +16,24 @@ var crit_hit_max_length := 6.0
 
 func _ready() -> void:
 	# Hide hitmarkers
-	for reg_hm in reg_hit_markers:
+	for reg_hm in reg_hitmarkers:
 		reg_hm.default_color.a = 0
-	for crit_hm in crit_hit_markers:
+	for crit_hm in crit_hitmarkers:
 		crit_hm.default_color.a = 0
 
 func _process(delta) -> void:
-	if reg_hit_markers[0].default_color.a > 0 or crit_hit_markers[0].default_color.a > 0:
+	if reg_hitmarkers[0].default_color.a > 0 or crit_hitmarkers[0].default_color.a > 0:
 		recent_dmg -= drain_rate * delta
 	else:
 		recent_dmg -= drain_rate * delta * 4
 	
 	recent_dmg = clampf(recent_dmg, 0, max_recent_dmg)
-	
-	print(recent_dmg)
 
 func on_regular_hit(damage: float) -> void:
-	display_hitmarker(damage, reg_hit_markers, reg_hit_max_length, reg_anim_player, "RegHit")
+	display_hitmarker(damage, reg_hitmarkers, reg_hit_max_length, reg_anim_player, "RegHit")
 
 func on_crit_hit(damage: float) -> void:
-	display_hitmarker(damage, crit_hit_markers, crit_hit_max_length, crit_anim_player, "CritHit")
+	display_hitmarker(damage, crit_hitmarkers, crit_hit_max_length, crit_anim_player, "CritHit")
 
 func display_hitmarker(damage: float, hitmarkers: Array[Line2D], hit_max_length: float, anim_player: AnimationPlayer, anim_name: String) -> void:
 	# Add damage to recent damage
