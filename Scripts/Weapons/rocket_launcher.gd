b37f87e6bba6ebc3960efb5639aeb23bdce6b241
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-07-16 23:39:43 +1000

    Added Rocket Projectile
    - Created Rocket CharacterBody3D scene with a visual mesh and collision shape.
    - Created Rocket script that moves at a constant speed and explodes on collision.
    - Updated Rocket Launcher script to instantiate a rocket and set its direction, speed and rotation.

--- Scripts/Weapons/rocket_launcher.gd
+++ Scripts/Weapons/rocket_launcher.gd
@@ -2,35 +2,22 @@ class_name RocketLauncher
 
 extends Weapon
 
-@export var explosion_scene = preload("res://Scenes/Weapons/Components/explosion.tscn")
-
-const RAY_RANGE := 2000.0
+@export var rocket_scene = preload("res://Scenes/Weapons/Components/rocket.tscn")
+@export var rocket_speed : float = 29.0
 
 func shoot() -> void:
 	# Call base method
 	super()
 	
-	var space_state = camera.get_world_3d().direct_space_state
-	var screen_centre = get_viewport().get_size() / 2
-	
-	var ray_origin = camera.project_ray_origin(screen_centre)
-	var ray_end = ray_origin + camera.project_ray_normal(screen_centre) * RAY_RANGE
-	
-	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end, HITSCAN_COLLISION_MASK)
-	query.collide_with_areas = true
-	
-	var result = space_state.intersect_ray(query)
-	
-	if result:
-		print("Hit: " + result.collider.name)
-		spawn_decal(result.get("position"), result.get("normal"))
-		
-		# Instantiate the explosion
-		var explosion = explosion_scene.instantiate()
-		# Make it a child of the level scene
-		var level = get_tree().get_first_node_in_group("level")
-		level.add_child(explosion)
-		# Set its position
-		explosion.global_position = result.position
-	else:
-		print ("Hit nothing.")
+	# Instantiate the rocket
+	var rocket = rocket_scene.instantiate() as CharacterBody3D
+	# Set its direction and speed
+	rocket.direction = -camera.get_global_transform().basis.z
+	rocket.speed = rocket_speed
+	# Make it a child of the level scene
+	var level = get_tree().get_first_node_in_group("level")
+	level.add_child(rocket)
+	# Set its position
+	rocket.global_position = camera.global_position
+	# Set its rotation
+	rocket.transform.basis = camera.global_transform.basis
