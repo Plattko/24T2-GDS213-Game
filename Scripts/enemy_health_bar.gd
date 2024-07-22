a340f7dac0f14b0b38031cf4033f46bd78e479d1
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-07-23 02:21:07 +1000

    Made Enemy Health Bar Naturally Hidden
    - Added Displayer Timer Timer node to Health Bar node in Robot scene.
    - Updated Enemy Health Bar to hide itself when the enemy spawns and display the health bar for 2 seconds when they take damage before hiding it again.
    - Fixed warning message in console when killing an enemy with an active tween by creating the tween as a child of the enemy instead of the tree.

--- Scripts/enemy_health_bar.gd
+++ Scripts/enemy_health_bar.gd
@@ -2,8 +2,12 @@ class_name EnemyHealthBar
 extends ProgressBar
 
 @export var dmg_bar : ProgressBar
+@export var display_timer : Timer
 
 var cur_health : float
+
+func _ready() -> void:
+	visible = false
 
 func init_health(health: float) -> void:
 	cur_health = health
@@ -15,6 +19,9 @@ func init_health(health: float) -> void:
 	dmg_bar.value = cur_health
 
 func update_health(new_health: float, is_crit: bool) -> void:
+	visible = true
+	display_timer.start()
+	
 	var prev_health : float = cur_health
 	cur_health = min(max_value, new_health)
 	value = cur_health
@@ -30,7 +37,7 @@ func update_health(new_health: float, is_crit: bool) -> void:
 			style_box.set("bg_color", Color(0.961, 0.717, 0.068))
 			dmg_bar.add_theme_stylebox_override("fill", style_box)
 		
-		var tween : Tween = get_tree().create_tween()
+		var tween : Tween = create_tween()
 		if is_crit:
 			tween.finished.connect(reset_dmg_bar_colour)
 		tween.tween_property(dmg_bar, "value", cur_health, 0.4)
@@ -42,3 +49,6 @@ func reset_dmg_bar_colour() -> void:
 	var style_box : StyleBoxFlat = dmg_bar.get_theme_stylebox("fill").duplicate()
 	style_box.set("bg_color", Color.WHITE)
 	dmg_bar.add_theme_stylebox_override("fill", style_box)
+
+func _on_display_timer_timeout():
+	visible = false
