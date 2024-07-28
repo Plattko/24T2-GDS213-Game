4aed097de9295420b8b168418fbe8150b6553b45
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-07-28 18:49:15 +1000

    Added Enemy Drop Position Animation
    - Move position of the drop navigation link in the Enemy Optimisation Testing scene.
    - Updated Drop Enemy State to inherit from Jump Enemy State.
    - Updated enter function in Drop Enemy State script to set the drop_height variable and updated references to msg.height to use drop_height instead.
    - Moved the landing functionality from the enter function to a new land function in Drop Enemy State script.
    - Overrode tween_jump function in Drop Enemy State script to call the land function instead of immediately transitioning to the run state.

--- Scripts/States/Enemy States/drop_enemy_state.gd
+++ Scripts/States/Enemy States/drop_enemy_state.gd
@@ -1,27 +1,40 @@
 class_name DropEnemyState
-extends EnemyState
+extends JumpEnemyState
 
+var drop_height : float
 var stun_time_multiplier : float = 0.1
 
-func enter(msg : Dictionary = {}) -> void:
-	if !multiplayer.is_server(): return
-	
-	if msg.link_exit_position:
-		enemy.global_position = msg.link_exit_position
-	
-	if msg.height and msg.height > stunnable_drop_height:
+func enter(msg: Dictionary = {}) -> void:
+	if multiplayer.is_server():
+		if !msg.height:
+			printerr("Missing drop height.")
+		# Set the drop height
+		drop_height = msg.height
+		jump_clearance = drop_height * 0.8
+	# Call the base function to start the drop
+	super(msg)
+
+func tween_jump() -> void:
+	# Create a tween to track the progress of the jump arc
+	var tween = get_tree().create_tween()
+	await tween.tween_method(jump, 0.0, 1.0, jump_time).finished
+	land()
+
+func land() -> void:
+	# Handle stunned landing
+	if drop_height > stunnable_drop_height:
 		print("Stunned landing.")
 		enemy.animate(enemy.Animations.STUNNED)
 		enemy.nav_agent.avoidance_enabled = false
 		enemy.velocity = Vector3.ZERO
 		
-		var stun_time = msg.height * 0.1
+		var stun_time = drop_height * 0.1
 		print("Stun time: " + str(stun_time))
 		await get_tree().create_timer(stun_time).timeout
 		enemy.nav_agent.avoidance_enabled = true
 		transition.emit("RunEnemyState")
+	# Handle regular landing
 	else:
 		print("Regular landing.")
-		await get_tree().physics_frame
 		enemy.velocity = Vector3.ZERO
 		transition.emit("RunEnemyState")
