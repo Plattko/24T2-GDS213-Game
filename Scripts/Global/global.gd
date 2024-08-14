14cbe7eacc974fef22ea071c4ec63967f1fbb517
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-08-14 18:00:36 +1000

    Updated Quitting Hotkey
    - Commented out the quit input in the Global script.
    - Added the quit input to the Game Manager script and updated it to close the game if the player's multiplayer peer is offline or close their multiplayer peer if it's online.

--- Scripts/Global/global.gd
+++ Scripts/Global/global.gd
@@ -2,8 +2,7 @@ extends Node
 
 var camera
 
-# TODO: Test if this quits other clients' games
-func _input(event):
-	# Handle quit
-	if event.is_action_pressed("quit"):
-		get_tree().quit()
+#func _input(event):
+	## Handle quit
+	#if event.is_action_pressed("quit"):
+		#get_tree().quit()
